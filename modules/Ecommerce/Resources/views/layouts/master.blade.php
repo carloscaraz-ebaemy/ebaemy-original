7be0d84abbd7e010970ec92f8008955c09079203
carloscaraz@ebaemy.com <carloscaraz@ebaemy.com>
2026-02-13 13:18:00 -0500

    modificacion del  seo

--- modules/Ecommerce/Resources/views/layouts/master.blade.php
+++ modules/Ecommerce/Resources/views/layouts/master.blade.php
@@ -2,94 +2,85 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+   <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <script src="{{ asset('porto-ecommerce/assets/js/jquery.min.js') }}"></script>
 
 @php
-
     $seo = \App\Models\Tenant\ConfigurationEcommerce::first() ?? new \App\Models\Tenant\ConfigurationEcommerce();
     $company = \App\Models\Tenant\Company::first();
     $social_scripts = \App\Models\Tenant\ConfigurationScript::where('active', true)->get();
     
     $path_logos = asset('storage/uploads/logos/');
-    $path_favicons = asset('storage/uploads/favicons/');
     $logo_default = asset('porto-ecommerce/assets/images/logo-black.png');
     $v = $company->updated_at ? $company->updated_at->timestamp : time();
 
-@endphp
-
-<title>{{ $seo->seo_title ?? $company->name ?? 'Tienda Online' }}</title>
-<script src="{{ asset('porto-ecommerce/assets/js/jquery.min.js') }}"></script>
-<meta name="description" content="{{ $seo->seo_description ?? 'Bienvenido a nuestra tienda. Encuentra los mejores productos aquí.' }}">
-<meta name="keywords" content="{{ $seo->seo_keywords ?? 'ecommerce, tienda, online' }}">
-<meta name="author" content="{{ $seo->seo_author ?? $company->name }}">
-
-@if($seo->indexable)
-    <meta name="robots" content="{{ $seo->seo_robots ?? 'index, follow' }}">
-@else
-    <meta name="robots" content="noindex, nofollow">
-@endif
-
-<link rel="canonical" href="{{ $seo->canonical_url ?? url()->current() }}">
-
-<meta property="og:title" content="{{ $seo->og_title ?? $seo->seo_title ?? $company->name }}">
-<meta property="og:description" content="{{ $seo->og_description ?? $seo->seo_description }}">
-<meta property="og:type" content="{{ $seo->og_type ?? 'website' }}">
-<meta property="og:url" content="{{ url()->current() }}">
-
-<meta property="og:image" content="{{ $seo->og_image ? $path_logos . '/' . $seo->og_image : $logo_default }}">
-
-<meta name="twitter:card" content="{{ $seo->twitter_card ?? 'summary_large_image' }}">
-<meta name="twitter:title" content="{{ $seo->twitter_title ?? $seo->seo_title ?? $company->name }}">
-<meta name="twitter:description" content="{{ $seo->twitter_description ?? $seo->seo_description }}">
-<meta name="twitter:image" content="{{ $seo->twitter_image ? $path_logos . '/' . $seo->twitter_image : ($seo->og_image ? $path_logos . '/' . $seo->og_image : $logo_default) }}">
-
-
-
-  <!-- Favicon -->
-    @if($company && $company->favicon)
-    @php
-        // Verificamos si la ruta ya tiene 'storage/' para no duplicarla
+    // Lógica inteligente para el Favicon (Pestaña del navegador)
+    if ($company && $company->favicon) {
         $favicon_url = str_contains($company->favicon, 'storage/') 
                        ? asset($company->favicon) 
-                       : asset('storage/' . $company->favicon);
-    @endphp
+                       : asset('storage/uploads/favicons/' . $company->favicon);
+    } else {
+        $favicon_url = asset('porto-ecommerce/assets/images/icons/favicon.ico');
+    }
+
+    // Lógica inteligente para la Imagen de Compartición (WhatsApp/Facebook)
+    if ($seo->og_image) {
+        $share_image = str_contains($seo->og_image, 'storage/') 
+                       ? asset($seo->og_image) 
+                       : asset('storage/uploads/logos/' . $seo->og_image);
+    } else {
+        $share_image = $favicon_url; // Respaldo dorado si no hay imagen SEO
+    }
+@endphp
+
+    <title>{{ $seo->seo_title ?? $company->name ?? 'Tienda Online' }}</title>
+
+    <meta name="description" content="{{ $seo->seo_description ?? 'Bienvenido a nuestra tienda.' }}">
+    <meta name="keywords" content="{{ $seo->seo_keywords ?? 'ecommerce, tienda' }}">
+    <meta name="author" content="{{ $seo->seo_author ?? $company->name }}">
+
+    @if($seo->indexable)
+        <meta name="robots" content="{{ $seo->seo_robots ?? 'index, follow' }}">
+    @else
+        <meta name="robots" content="noindex, nofollow">
+    @endif
+
+    <link rel="canonical" href="{{ $seo->canonical_url ?? url()->current() }}">
+
+    <meta property="og:title" content="{{ $seo->og_title ?? $seo->seo_title ?? $company->name }}">
+    <meta property="og:description" content="{{ $seo->og_description ?? $seo->seo_description }}">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:image" content="{{ $share_image }}?v={{ $v }}">
+    <meta property="og:image:secure_url" content="{{ $share_image }}?v={{ $v }}">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="{{ $seo->twitter_title ?? $seo->seo_title ?? $company->name }}">
+    <meta name="twitter:description" content="{{ $seo->twitter_description ?? $seo->seo_description }}">
+    <meta name="twitter:image" content="{{ $share_image }}?v={{ $v }}">
+
     <link rel="icon" type="image/png" href="{{ $favicon_url }}?v={{ $v }}">
     <link rel="apple-touch-icon" href="{{ $favicon_url }}?v={{ $v }}">
     <link rel="shortcut icon" href="{{ $favicon_url }}?v={{ $v }}">
-@endif
-
 
     @foreach($social_scripts->where('position', 'head') as $item)
         {!! $item->script !!}
     @endforeach
 
-    @if($social_scripts->where('position', 'head')->isNotEmpty())
-            @foreach($social_scripts->where('position', 'head') as $item)
-                @if(!empty($item->script))
-                    {!! $item->script !!}
-                @endif
-            @endforeach
-        @endif
-
-  
-
-    <!-- Plugins CSS File -->
     <link rel="stylesheet" href="{{ asset('porto-ecommerce/assets/css/bootstrap.min.css') }}">
-
-    <!-- Main CSS File -->
     <link rel="stylesheet" href="{{ asset('porto-ecommerce/assets/css/style.min.css') }}">
     <link rel="stylesheet" href="{{ asset('porto-ecommerce/assets/css/custom.css') }}">
     <link rel="stylesheet" href="{{ asset('porto-ecommerce/assets/css/rating.css') }}">
 
     @vite('resources/js/app.js')
 
-    <!-- Fontawesome -->
     <link rel="stylesheet" href="{{ asset('porto-ecommerce/assets/font-awesome/css/fontawesome-all.min.css') }}">
-    <!-- Estilos personalizados -->
     <link rel="stylesheet" href="{{ asset('porto-light/css/styles_ecommerce.css') }}" />
 </head>
 
